64ad2185e96eeeea1be641e7d76ba0c6f05c6a4c
Moses <moses.junior@student.moringaschool.com>
2021-12-21 10:08:59 +0300

    addition of routes between components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HoodsComponent } from './hoods/hoods.component';
+import { HoodComponent } from './hood/hood.component';
+import { NewHoodComponent } from './new-hood/new-hood.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'hoods',component:HoodsComponent},
+  {path:'hood',component:HoodComponent},
+  {path:'new-hood',component:NewHoodComponent},
+  {path:'profile',component:ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
